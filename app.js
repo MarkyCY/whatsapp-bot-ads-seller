5e1f86a047f9aeb8e8ca94f1a69e57888bc15a58
Marcos J. Cardenas P <oranmarcos@gmail.com>
2023-09-28 07:49:32 -0400

    Add API send message

--- app.js
+++ app.js
@@ -1,70 +1,33 @@
 const { createBot, createProvider, createFlow, addKeyword } = require('@bot-whatsapp/bot')
+const express = require('express')
+const bodyParser = require("body-parser");
 
 const QRPortalWeb = require('@bot-whatsapp/portal')
 const BaileysProvider = require('@bot-whatsapp/provider/baileys')
 const MockAdapter = require('@bot-whatsapp/database/mock')
 
-const flowSecundario = addKeyword(['2', 'siguiente']).addAnswer(['📄 Aquí tenemos el flujo secundario'])
+const { EVENTS } = require('@bot-whatsapp/bot')
 
-const flowDocs = addKeyword(['doc', 'documentacion', 'documentación']).addAnswer(
-    [
-        '📄 Aquí encontras las documentación recuerda que puedes mejorarla',
-        'https://bot-whatsapp.netlify.app/',
-        '\n*2* Para siguiente paso.',
-    ],
-    null,
-    null,
-    [flowSecundario]
-)
 
-const flowTuto = addKeyword(['tutorial', 'tuto']).addAnswer(
-    [
-        '🙌 Aquí encontras un ejemplo rapido',
-        'https://bot-whatsapp.netlify.app/docs/example/',
-        '\n*2* Para siguiente paso.',
-    ],
-    null,
-    null,
-    [flowSecundario]
-)
+const flowBienvenida = addKeyword(EVENTS.WELCOME)
+    .addAnswer('Bienvenido a este chatbot')
+    .addAnswer([
+        'Escribe cualquiera de los comandos:',
+        '*info* para...',
+        '*agente* para...',
+    ])
+    .addAction(async(ctx) => {
 
-const flowGracias = addKeyword(['gracias', 'grac']).addAnswer(
-    [
-        '🚀 Puedes aportar tu granito de arena a este proyecto',
-        '[*opencollective*] https://opencollective.com/bot-whatsapp',
-        '[*buymeacoffee*] https://www.buymeacoffee.com/leifermendez',
-        '[*patreon*] https://www.patreon.com/leifermendez',
-        '\n*2* Para siguiente paso.',
-    ],
-    null,
-    null,
-    [flowSecundario]
-)
+        console.log(ctx)
+        
+    })
 
-const flowDiscord = addKeyword(['discord']).addAnswer(
-    ['🤪 Únete al discord', 'https://link.codigoencasa.com/DISCORD', '\n*2* Para siguiente paso.'],
-    null,
-    null,
-    [flowSecundario]
-)
-
-const flowPrincipal = addKeyword(['hola', 'ole', 'alo'])
-    .addAnswer('🙌 Hola bienvenido a este *Chatbot*')
-    .addAnswer(
-        [
-            'te comparto los siguientes links de interes sobre el proyecto',
-            '👉 *doc* para ver la documentación',
-            '👉 *gracias*  para ver la lista de videos',
-            '👉 *discord* unirte al discord',
-        ],
-        null,
-        null,
-        [flowDocs, flowGracias, flowTuto, flowDiscord]
-    )
+const app = express();
+app.use(bodyParser.json());
 
 const main = async () => {
     const adapterDB = new MockAdapter()
-    const adapterFlow = createFlow([flowPrincipal])
+    const adapterFlow = createFlow([flowBienvenida])
     const adapterProvider = createProvider(BaileysProvider)
 
     createBot({
@@ -74,6 +37,20 @@ const main = async () => {
     })
 
     QRPortalWeb()
-}
+
+    /*
+        Api de envio de mensajes
+    */
+    app.post("/send-message-bot", async (req, res) => {
+    const num = req.body.number;
+    const text = req.body.text;
+    await adapterProvider.sendText(`${num}@c.us`, text);
+    res.send({ data: "enviado!" });
+  });
+
+      const PORT = 4000;
+      app.listen(PORT, () => console.log(`Servidor API: http://localhost:${PORT}`));
+    };
+    
 
 main()
