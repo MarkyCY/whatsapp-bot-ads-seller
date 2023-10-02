2c1710ff2f55ce6819b3cb853f8331689aaa4c1b
Marcos J. Cardenas P <oranmarcos@gmail.com>
2023-10-02 17:05:53 -0400

    Add mongo module

---
+++ module/mongo.js
@@ -0,0 +1,41 @@
+const { MongoClient, ServerApiVersion } = require('mongodb');
+
+const mongo_user = process.env.USER_MONGO;
+const mongo_pass = process.env.PASS_MONGO;
+
+// URL de conexión a la base de datos MongoDB
+const url = `mongodb+srv://${mongo_user}:${mongo_pass}@cluster0.nipfwih.mongodb.net/?retryWrites=true&w=majority`;
+
+// Nombre de la base de datos
+const dbName = 'shop';
+
+// Crea una instancia del cliente de MongoDB
+const client = new MongoClient(url, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+// Función para conectar a la base de datos
+async function run() {
+  try {
+    await client.connect();
+    
+  } catch (err) {
+    console.error('Error al conectar a la base de datos:', err);
+    throw err;
+  }
+}
+
+// Función para obtener la instancia de la base de datos
+function getDb() {
+  return client.db(dbName);
+}
+
+// Exporta las funciones para su uso en otros módulos
+module.exports = {
+  run,
+  getDb,
+};
